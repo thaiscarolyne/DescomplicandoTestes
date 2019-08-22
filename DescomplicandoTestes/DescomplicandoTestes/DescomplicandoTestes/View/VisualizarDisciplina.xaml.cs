815964a2156c354d3c81299fb9a1121d2b2c1607
Thais Almeida <Thaiscarolyne_itba@hotmail.com>
2019-08-22 13:36:39 -0300

    Tela VisualizarDisciplina finalizada

--- DescomplicandoTestes/DescomplicandoTestes/DescomplicandoTestes/View/VisualizarDisciplina.xaml.cs
+++ DescomplicandoTestes/DescomplicandoTestes/DescomplicandoTestes/View/VisualizarDisciplina.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +17,24 @@ namespace DescomplicandoTestes.View
 			InitializeComponent ();
 
             BindingContext = new ViewModel.VisualizarDisciplinaViewModel();
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var height = (double)(mainDisplayInfo.Height / mainDisplayInfo.Density);
+
+            if (height < 550)
+            {
+                LabelAdicionarConteudo.FontSize = 17;
+            }
+
+            if (height > 550 && height < 650)
+            {
+                LabelAdicionarConteudo.FontSize = 18;
+            }
+
+            if (height > 650)
+            {
+                LabelAdicionarConteudo.FontSize = 20;
+            }
         }
 	}
 }
